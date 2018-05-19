3891fd0f056a779f299a2d1dee8bd4b69fd6b815
ritz.wu@outlook.com Wu <ritz.wu@outlook.com>
2018-05-19 16:48:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,17 @@ function logShout(string){
   var shoutWord = string.toUppercase();
   console.log(shoutWord);
 }
+
+function logWhisper(string){
+  var whisperWord = string.toLowerCase();
+  console.log(whisperWord);
+}
+
+function sayHiToGrandma(string){
+  if (string !=== uppercase){
+    return "I can't hear you!";
+  }
+  else {
+    return "YES INDEED!"
+  }
+}
