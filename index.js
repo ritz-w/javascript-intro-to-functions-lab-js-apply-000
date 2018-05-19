86ee4894020b3b67481ae28669ee711e88c69e2f
ritz.wu@outlook.com Wu <ritz.wu@outlook.com>
2018-05-19 16:43:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function shout(string) {
   return string.toUpperCase();
-};
+}
 
+function whisper(string){
+  return string.toLowerCase();
+}
 
+function logShout(string){
+  var whisperedWord = string.toLowerCase();
+  console.log(whisperedWord);
+}
